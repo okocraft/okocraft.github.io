d66f3b82a643a138252b4e3ded788b5da8804481
Siroshun09 <Siroshun09@users.noreply.github.com>
2021-03-19 13:23:32 +0000

    deploy: okocraft/homepage@096c19f6f50a38a244fa1bbc29ac394a7945ba54

--- _nuxt/static/1616159655/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616159655",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d720b7c0"}}}}(null,"\u002F"));
