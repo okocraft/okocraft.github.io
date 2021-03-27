c4f09500dbf08c8916cfab34b9a2f58f1e283446
Siroshun09 <Siroshun09@users.noreply.github.com>
2021-03-27 14:22:01 +0000

    deploy: okocraft/homepage@d0bb2778214ab715889b08cb051814fbd5d4b853

--- _nuxt/static/1616167922/rule/building-clause/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616167922",layout:"default",error:a,serverRendered:true,routePath:"\u002Frule\u002Fbuilding-clause",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"41ce1ba3"}}}}(null));
--- _nuxt/static/1616167922/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616167922",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"41ce1ba3"}}}}(null,"\u002F"));
