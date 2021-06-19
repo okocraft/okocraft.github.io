2e4f4379e36f465a81a93bd3aa9ba700b6c7e425
Siroshun09 <Siroshun09@users.noreply.github.com>
2021-06-19 10:30:16 +0000

    deploy: okocraft/homepage@6d6073e4322f5e96bbc46a26fc94e69101afc53f

--- _nuxt/static/1616855008/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FDiscord","\u002FRule","\u002Frule","\u002Fdiscord","\u002Frule\u002Fterms-of-service","\u002Frule\u002Fpenalty-clause","\u002Frule\u002Fbuilding-clause"]})
